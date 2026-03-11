7cde57e274847535d53e6a8ede3576bfc1d8ef52
sor999 <sorsor999@naver.com>
2026-03-11 23:13:57 +0900

    fix: pip install 시 ImportError 해결

--- ros/src/ros_kafka_bridge/setup.py
+++ ros/src/ros_kafka_bridge/setup.py
@@ -15,12 +15,12 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'confluent-kafka'],
     zip_safe=True,
     maintainer='sor999',
     maintainer_email='sorsor999@naver.com',
     description='ROS2-Kafka 브릿지: 텔레메트리 Produce 및 Command Consume/Relay',
-    license='TODO: License declaration',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
